b3b3fbc5baced4a645e99d32f8316e9bc7d2f012
quyen <22010342@st.phenikaa-uni.edu.vn>
2025-02-18 02:36:27 +0700

    thêm_nút_play_pause_next_prev

--- Group12Project/Music_app/lib/ui/now_playing/playing.dart
+++ Group12Project/Music_app/lib/ui/now_playing/playing.dart
@@ -141,17 +141,41 @@ class _NowPlayingPageState extends State<NowPlayingPage>
               ),
               Padding(
                 padding: const EdgeInsets.only(
-                  top: 32,
+                  top: 0,
                   left: 24,
                   right: 24,
-                  bottom: 16,
+                  bottom: 10,
                 ),
                   child: _progressBar(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 0,
+                  left: 24,
+                  right: 24,
+                  bottom: 10,
+                ),
+                child: _mediaButtons(),
               )
             ],
           ),
         ),
       )
+    );
+  }
+
+  Widget _mediaButtons() {
+    return const SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          MediaButtonControl(function: null, icon: Icons.shuffle, color: Colors.deepPurple, size: 24),
+          MediaButtonControl(function: null, icon: Icons.skip_previous, color: Colors.deepPurple, size: 36),
+          MediaButtonControl(function: null, icon: Icons.play_arrow_sharp, color: Colors.deepPurple, size: 48),
+          MediaButtonControl(function: null, icon: Icons.skip_next, color: Colors.deepPurple, size: 36),
+          MediaButtonControl(function: null, icon: Icons.repeat, color: Colors.deepPurple, size: 24),
+        ],
+      ),
     );
   }
 
@@ -167,3 +191,36 @@ class _NowPlayingPageState extends State<NowPlayingPage>
         });
   }
 }
+
+class MediaButtonControl extends StatefulWidget {
+  const MediaButtonControl({
+    super.key,
+    required this.function,
+    required this.icon,
+    required this.color,
+    required this. size,
+});
+
+  final void Function()? function;
+  final IconData icon;
+  final double? size;
+  final Color? color;
+
+
+  @override
+  State<StatefulWidget> createState() => _MediaButtonControlState();
+
+}
+class _MediaButtonControlState extends State<MediaButtonControl> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: widget.function,
+      icon: Icon(widget.icon),
+      iconSize: widget.size,
+      color: widget.color ?? Theme.of(context).colorScheme.primary,
+    );
+  }
+}
+
+
