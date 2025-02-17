e230560406c4a3d0169a0a8ca7d12d40d87daa65
quyen <22010342@st.phenikaa-uni.edu.vn>
2025-02-18 00:50:00 +0700

    title_image_text_nowPlaying

--- Group12Project/Music_app/lib/ui/now_playing/playing.dart
+++ Group12Project/Music_app/lib/ui/now_playing/playing.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../data/model/song.dart';
@@ -25,14 +26,118 @@ class NowPlayingPage extends StatefulWidget {
   @override
   State<NowPlayingPage> createState() => _NowPlayingPageState();
 }
+//Text header
+class _NowPlayingPageState extends State<NowPlayingPage>
+      with SingleTickerProviderStateMixin {
+  late AnimationController _imageAnimController;
 
-class _NowPlayingPageState extends State<NowPlayingPage> {
+  @override
+  void initState(){
+    super.initState();
+    _imageAnimController = AnimationController(
+      vsync: this,
+      duration: const Duration(microseconds: 12000),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Now Playing'),
+    // return const Scaffold(
+    //   body: Center(
+    //     child: Text('Now Playing'),
+    //   ),
+    // );
+    final screenWidth = MediaQuery.of(context).size.width;
+    const delta = 64;
+    final radius = (screenWidth - delta) / 2;
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text(
+          'Now Playing',
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.more_horiz),
+        ),
       ),
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(widget.playingSong.album),
+              const SizedBox
+                (height: 16,
+              ),
+              const Text('_ ___ _'),
+              const SizedBox(
+                height: 48,
+              ),
+              RotationTransition(turns: Tween(begin: 0.0, end: 1.0).animate(_imageAnimController),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(radius),
+                  child: FadeInImage.assetNetwork(
+                      placeholder: 'assets/ITunes_logo.svg.png',
+                      image: widget.playingSong.image,
+                      width: screenWidth - delta,
+                     height: screenWidth - delta,
+                     imageErrorBuilder: (context, error, stackTrace) {
+                       return Image.asset('assets/ITunes_logo.svg.png',
+                       width: screenWidth - delta,
+                       height: screenWidth - delta,
+                       );
+                     },
+                  ),
+               ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 64, bottom: 16),
+                child: SizedBox(child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(onPressed: () {},
+                        icon: const Icon(Icons.share_outlined),
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    Column(
+                        children: [
+                          Text(widget.playingSong.title,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium!
+                                    .color),
+                          ),
+                          const  SizedBox(height: 8,),
+                          Text(
+                            widget.playingSong.artist,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                            color: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .color),
+                          )
+                        ],
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite_outline),
+                      color: Theme.of(context).colorScheme.primary,
+
+                    )
+                  ],
+                ),),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
