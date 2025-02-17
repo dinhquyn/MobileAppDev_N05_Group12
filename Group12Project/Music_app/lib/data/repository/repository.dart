0cceaf8903c481d896d360d8c50005e96e6ece5e
phantinh2004xz <22010122@st.phenikaa-uni.edu.vn>
2025-02-17 13:35:20 +0700

    load dữ liệu local

--- Group12Project/Music_app/lib/data/repository/repository.dart
+++ Group12Project/Music_app/lib/data/repository/repository.dart
@@ -1,5 +1,4 @@
 import 'package:appdev/data/source/source.dart';
-
 import '../model/song.dart';
 
 abstract interface class Repository {
@@ -12,18 +11,14 @@ class DefaultRepository implements Repository {
 
   @override
   Future<List<Song>?> loadData() async {
-    List<Song> songs = [];
-    await _remoteDataSource.loadData().then((remoteSongs) {
-      if (remoteSongs == null) {
-        _localDataSource.loadData().then((localSongs) {
-          if (localSongs != null) {
-            songs.addAll(localSongs);
-          }
-        });
-      } else {
-        songs.addAll(remoteSongs);
-      }
-    });
-    return songs;
+    // Lấy dữ liệu từ remote trước
+    final remoteSongs = await _remoteDataSource.loadData();
+    if (remoteSongs != null) {
+      return remoteSongs;
+    }
+    
+    // Nếu remote không có dữ liệu, lấy dữ liệu từ local
+    final localSongs = await _localDataSource.loadData();
+    return localSongs;
   }
 }
